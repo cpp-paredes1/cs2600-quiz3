12ecff360893f7ce93701725186ee084b834d113
minnowfeather <matthewparedes2k3@gmail.com>
2022-10-19 19:50:07 -0700

    Added test cases to EmployeeMain.c

--- EmployeeMain.c
+++ EmployeeMain.c
@@ -20,6 +20,9 @@ int main(void){
 
     PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char * nameToFind); 
 
+    PtrToEmployee searchEmployeeByPhoneNumber(const Employee table[], int sizeTable, char * phoneToFind);
+
+    PtrToEmployee searchEmployeeBySalary(const Employee table[], int sizeTable, double salaryToFind);
  
 
     //defined in employeeTable.c 
@@ -60,7 +63,48 @@ int main(void){
 
         printf("Employee Tony Bobcat is NOT found in the record\n"); 
 
-     
+    
+
+    // TESTING EmployeeSearch.c
+    // Test 1
+    // Expected: pointer to Employee Dorris Perl
+    matchPtr = searchEmployeeByPhoneNumber(EmployeeTable, EmployeeTableEntries, "310-555-1215");
+    if(matchPtr != NULL){
+        printf("Found Employee %s, with phone number %s.", matchPtr->name, matchPtr->phone);
+    } else {
+        printf("Test 1: employee not found.");
+    }
+    
+    // Test 2
+    // Expected: NULL
+    matchPtr = searchEmployeeByPhoneNumber(EmployeeTable, EmployeeTableEntries, "111-111-1201");
+    if(matchPtr != NULL){
+        printf("Found Employee %s, with phone number %s.", matchPtr->name, matchPtr->phone);
+    } else {
+        printf("Test 2: employee not found.");
+    }
+
+    // Test 3
+    // Expected: pointer to Employee Tammy Franklin
+    matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 4.50);
+    if(matchPtr != NULL){
+        printf("Found Employee %s, with salary %s.", matchPtr->name, matchPtr->salary);
+    } else {
+        printf("Test 3: employee not found.");
+    }
+
+    // Test 4
+    // Expected: NULL
+    matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 12.12);
+    if(matchPtr != NULL){
+        printf("Found Employee %s, with salary %s.", matchPtr->name, matchPtr->salary);
+    } else {
+        printf("Test 3: employee not found.");
+    }
+    
+    
+    
+
 
     return EXIT_SUCCESS; 
 
