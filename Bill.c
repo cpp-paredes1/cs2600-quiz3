d6269cb9bb60790d55bed58dc2e7c5bf79457fe0
minnowfeather <matthewparedes2k3@gmail.com>
2022-10-22 03:48:36 -0700

    Outline created

---
+++ Bill.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int size, char * args){
+	// init random object
+
+	// generate random food
+
+	// parse user tax
+
+	// parse user tip
+
+	// total = price * (1 + tax) * (1 + tip);
+}
