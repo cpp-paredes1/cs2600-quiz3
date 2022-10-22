6798d7a7cc2d408984c21efcb1237a804a2d108e
minnowfeather <matthewparedes2k3@gmail.com>
2022-10-22 05:14:00 -0700

    Completed to my best ability

--- numGuess.c
+++ numGuess.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 int maxNum;
+char newLineHolder;
+
+void printMenu(){
+	printf("Press 1 to play a game\nPress 2 to change the max number\nPress 3 to quit\n");
+}
 
 void numGuess(int target){
+	// printf("%i", target);
 	// track game state with boolean
 	bool guessed = false;
+	// create int to hold user input
+	int userguess;
+	// create int to hold return value of scanf
+	int valid;
 	// while number isnt guessed, keep asking user for inputs
+	while(!guessed){
+		// clear newline character which is left behind in the buffer
+		scanf("%c", &newLineHolder);
 
+		printf("Input a guess: ");
+		// valid = 1 if input is an int. valid = 0 if it is not
+		valid = scanf("%i", &userguess);
+		// if input was not an int, exit
+		if(valid == 0){
+			guessed = true;
+		} else {
+			if(userguess == target){
+				printf("You got the right number!\n");
+				guessed = true;
+			}
+			else if(userguess > target){
+				printf("Your guess was too high.\n");
+			} else{
+				printf("Your guess was too low.\n");
+			}
+		}
+	}
 	// once they are done, exit the function
 	// user may choose to play again 
 }
@@ -27,17 +59,23 @@ int main(){
 	// keep playing while game not finished
 	while(!gameFinished){
 		printMenu();
-		sscanf("> ", &answer);
+		printf("> ");
+		scanf("%i", &answer);
 		switch(answer){
 			case 1:
 				numGuess((rand()%maxNum)+1);
 				break;
 			case 2:
-				sscanf("What would you like the new max to be?: ", &maxNum);
+				printf("What would you like the new max to be: ");
+				scanf("%i", &maxNum);
+				scanf("%c", &newLineHolder);
 				break;
 			case 3:
 				gameFinished = true;
 				break;
+			default:
+				break;
 		}
 	}
+	return EXIT_SUCCESS;
 }
